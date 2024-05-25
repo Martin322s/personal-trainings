d39c89a0a7402b35a77e5fd48a93bc1588447957
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-25 13:49:42 +0300

    odd-positions

---
+++ 11-odd-numbers.js
@@ -0,0 +1,14 @@
+function solve(array) {
+    let resultArr = [];
+    array = array.map(x => x * 2);
+
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 !== 0) {
+            resultArr.push(array[i]);
+        }
+    }
+
+    console.log(resultArr.reverse().join(' '));
+}
+
+solve([10, 15, 20, 25]);
