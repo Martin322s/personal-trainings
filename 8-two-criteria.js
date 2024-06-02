b0d28f5b5ca70c937f68600b9b8c73a5d7cabf90
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-02 14:24:29 +0300

    array-two-criteria

---
+++ 8-two-criteria.js
@@ -0,0 +1,6 @@
+function solve(input) {
+    let secondCriteria = input.sort((a, b) => a.localeCompare(b));
+    let result = secondCriteria.sort((a, b) => a.length - b.length);
+
+    console.log(result.join('\n'));
+}
