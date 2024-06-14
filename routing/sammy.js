cd781b843c0631324a0267e549652504733fb4ea
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-14 20:20:49 +0300

    define about route

--- routing/sammy.js
+++ routing/sammy.js
@@ -3,6 +3,9 @@ $(function() {
         this.get('/home', () => {
             $('#root').html('<h1>Home page</h1>');
         });
+        this.get('/about', () => {
+            $('#root').html('<h1>About page</h1>');
+        });
     });
 
     app.run();
