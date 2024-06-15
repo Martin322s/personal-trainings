bdfeb8ee863ec342d754cae562c61045d67455e3
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-15 20:21:48 +0300

    define contacts route

--- routing/sammy.js
+++ routing/sammy.js
@@ -6,6 +6,9 @@ $(function() {
         this.get('/about', () => {
             $('#root').html('<h1>About page</h1>');
         });
+        this.get('/contacts', () => {
+            $('#root').html('<h1>Contacts page</h1>');
+        });
     });
 
     app.run();
