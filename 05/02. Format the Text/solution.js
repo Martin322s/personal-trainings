db9386940442a2504582ac701786170430de50c9
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-18 13:28:36 +0300

    format the sentences

--- 05/02. Format the Text/solution.js
+++ 05/02. Format the Text/solution.js
@@ -1,4 +1,8 @@
 function solve() {
     const inputText = document.getElementById('input').innerText;
     const outputDiv = document.getElementById('output');
+    const sentences = inputText
+        .split('.')
+        .filter(sentence => sentence.trim().length > 0)
+        .map(sentence => sentence.trim() + '.');
 }
