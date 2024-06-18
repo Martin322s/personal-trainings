8be248e2e36df6fbaf5379f1275fb1f7a8aa5c60
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-18 13:29:08 +0300

    create and append paragraphs

--- 05/02. Format the Text/solution.js
+++ 05/02. Format the Text/solution.js
@@ -5,4 +5,12 @@ function solve() {
         .split('.')
         .filter(sentence => sentence.trim().length > 0)
         .map(sentence => sentence.trim() + '.');
+
+    for (let i = 0; i < sentences.length; i += 3) {
+        const paragraph = document.createElement('p');
+        const textChunk = sentences.slice(i, i + 3).join(' ');
+        
+        paragraph.innerText = textChunk;
+        outputDiv.appendChild(paragraph);
+    }
 }
