aa090619b25cc14f8e8a5ca44dcffead558e2d43
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-18 13:32:26 +0300

    double the current font-size dynamically

--- 05/03. Growing Word/solution.js
+++ 05/03. Growing Word/solution.js
@@ -4,4 +4,7 @@ function growingWord() {
     if (!paragraph.style.fontSize) {
         paragraph.style.fontSize = '2px';
     }
+
+    let currentFontSize = parseInt(paragraph.style.fontSize);
+    paragraph.style.fontSize = (currentFontSize * 2) + 'px';
 }
