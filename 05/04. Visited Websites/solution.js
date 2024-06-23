9a85236fc956ad5ce74ea9bf064b242a3378a670
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-23 13:40:03 +0300

    count visited links

--- 05/04. Visited Websites/solution.js
+++ 05/04. Visited Websites/solution.js
@@ -5,6 +5,11 @@ function solve() {
 		link.addEventListener('click', (event) => {
 			event.preventDefault();
 			
+			const paragraph = link.nextElementSibling;
+			let countText = paragraph.innerText;
+			let count = Number(countText.match(/\d+/)[0]);
+			count++;
+			paragraph.innerText = `visited ${count} times`;
 		});
 	});
 }
