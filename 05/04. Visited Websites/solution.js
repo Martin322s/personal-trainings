b8aeca76af0051fed2d1ce90d52c9eff7831868c
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-23 13:39:24 +0300

    add custom event to the links

--- 05/04. Visited Websites/solution.js
+++ 05/04. Visited Websites/solution.js
@@ -1,4 +1,10 @@
 function solve() {
 	const links = document.querySelectorAll('.link-1 a');
 	
+	links.forEach(link => {
+		link.addEventListener('click', (event) => {
+			event.preventDefault();
+			
+		});
+	});
 }
