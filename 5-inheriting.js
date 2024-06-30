a5abb4f20375f649d6761ac8dfac83ac7ca95ed1
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-30 11:18:54 +0300

    person-teacher-extended

---
+++ 5-inheriting.js
@@ -0,0 +1,40 @@
+function solve() {
+    class Person {
+        constructor(name, email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        toString() {
+            console.log(`Person (name: ${this.name}, email: ${this.email})`);
+        }
+    }
+
+    class Teacher extends Person {
+        constructor(name, email, subject) {
+            super(name, email);
+            this.subject = subject;
+        }
+
+        toString() {
+            console.log(`Teacher (name: ${this.name}, email: ${this.email}, subject: ${this.subject})`);
+        }
+    }
+
+    class Student extends Person {
+        constructor(name, email, course) {
+            super(name, email);
+            this.course = course;
+        }
+
+        toString() {
+            console.log(`Person (name: ${this.name}, email: ${this.email}, course: ${this.course})`);
+        }
+    }
+
+    return {
+        Person,
+        Teacher,
+        Student
+    }
+}
