eb5d2a4904602020c3debad9c15b0640b86ccc28
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-06-30 13:46:32 +0300

    list-processor

--- 07/1-order-rectangles.js
+++ 07/1-order-rectangles.js
@@ -23,5 +23,3 @@ function solve(input) {
         console.log(input);
     return input;
 }
-
-solve([[1,20],[20,1],[5,3],[5,3]]);
---
+++ 07/2-list-processor.js
@@ -0,0 +1,27 @@
+function solve(input) {
+    let result = [];
+
+    function inner() {
+        for (let command of input) {
+            let [type, string] = command.split(' ');
+
+            switch(type) {
+                case 'add': result.push(string);
+                    break;
+                case 'remove': {
+                    while(result.includes(string)) {
+                        let index = result.indexOf(string);
+                        result.splice(index, 1);
+                    }
+                }
+                    break;
+                case 'print': console.log(result.join(','));
+                    break;
+            }
+        }
+    }
+
+    inner();
+}
+
+solve(['add peter', 'add george', 'add peter', 'remove peter','print']);
