45deb537bdc73502a8c93c2c2cc1f88a38e577a4
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-07-02 18:48:30 +0300

    new-customer-method

--- JS-Advnaced-2020/bank.js
+++ JS-Advnaced-2020/bank.js
@@ -1,5 +1,28 @@
 class Bank {
     constructor(bankName) {
-        this.bankName = bankName;
+        this._bankName = bankName;
+        this.allCustomers = [];
+    }
+
+    newCustomer(customer) {
+        if (this.allCustomers.some(x => x.firstName === customer.firstName)) {
+            throw new Error(`${customer.firstName} ${customer.lastName} is already our customer!`);
+        } else {
+            this.allCustomers.push(customer);
+            return customer;
+        }
     }
 }
+
+let bank = new Bank('SoftUni Bank');
+
+console.log(bank.newCustomer({firstName: 'Svetlin', lastName: 'Nakov', personalId: 6233267}));
+console.log(bank.newCustomer({firstName: 'Mihaela', lastName: 'Mileva', personalId: 4151596}));
+
+// bank.depositMoney(6233267, 250);
+// console.log(bank.depositMoney(6233267, 250));
+// bank.depositMoney(4151596,555);
+
+// console.log(bank.withdrawMoney(6233267, 125));
+
+// console.log(bank.customerInfo(6233267));
