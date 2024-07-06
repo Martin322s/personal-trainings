36376ca32fac9403a50c0d1c88d026083364d1e0
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-07-06 19:27:28 +0300

    customer-info

--- JS-Advnaced-2020/bank.js
+++ JS-Advnaced-2020/bank.js
@@ -8,7 +8,7 @@ class Bank {
         if (this.allCustomers.some(x => x.firstName === customer.firstName)) {
             throw new Error(`${customer.firstName} ${customer.lastName} is already our customer!`);
         } else {
-            this.allCustomers.push(customer);
+            this.allCustomers.push({ ...customer, info: [] });
             return customer;
         }
     }
@@ -20,6 +20,7 @@ class Bank {
             throw new Error('We have no customer with this ID!');
         } else {
             customer.totalMoney ? customer.totalMoney += amount : customer.totalMoney = amount;
+            customer.info.push(`${customer.info.length + 1}. ${customer.firstName} ${customer.lastName} made deposit of ${amount}$!`);
             return `${customer.totalMoney}$`;
         }
     }
@@ -31,10 +32,38 @@ class Bank {
             throw new Error('We have no customer with this ID!');
         } else {
             if (customer.totalMoney < amount) {
-                throw new Error(`${customer.firstName} ${customer.lastName} does not have enough money to withdraw that amount!”`)
+                throw new Error(`${customer.firstName} ${customer.lastName} does not have enough money to withdraw that amount!`)
             } else {
                 customer.totalMoney -= amount;
+                customer.info.push(`${customer.info.length + 1}. ${customer.firstName} ${customer.lastName} withdrew ${amount}$!`);
                 return `${customer.totalMoney}$`;
+            }
+        }
+    }
+
+    customerInfo(personalId) {
+        let customer = this.allCustomers.find(x => x.personalId === personalId);
+
+        if (!customer) {
+            throw new Error('We have no customer with this ID!');
+        } else {
+            customer.info = customer.info.sort((a, b) => {
+                let orderA = Number(a.split('.')[0]);
+                let orderB = Number(b.split('.')[0]);
+
+                return orderB - orderA;
+            });
+
+            let reuslt = [];
+            reuslt.push(this._bankName);
+            reuslt.push(`Customer name: ${customer.firstName} ${customer.lastName}`);
+            reuslt.push(`Customer ID: ${customer.personalId}`);
+            reuslt.push(`Total money: ${customer.totalMoney}$`);
+            reuslt.push('Transactions:');
+            reuslt.push(customer.info.join('\n'));
+
+            for (let key in customer) {
+                console.log(key);
             }
         }
     }
@@ -51,4 +80,4 @@ bank.depositMoney(4151596,555);
 
 console.log(bank.withdrawMoney(6233267, 125));
 
-// console.log(bank.customerInfo(6233267));
+console.log(bank.customerInfo(6233267));
