6578654b5246990b98baaab1952adc9bca537dcc
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-07-06 16:02:33 +0300

    test with object

--- unit-testing/tests/1-print-array.test.js
+++ unit-testing/tests/1-print-array.test.js
@@ -2,24 +2,26 @@ import { solve } from '../exercises/1-print-array-delimiter.js';
 import { assert, expect } from 'chai';
 
 describe('Print array with delimiter', () => {
-    it('Should print array with delimiter', () => {
+    it('Unit test on function with correct input', () => {
         let array = [1, 2, 3, 4];
         let delimiter = '-';
-
         let result = solve(array, delimiter);
         assert.equal(result, '1-2-3-4');
     });
 
-    it('Should print array with empty delimiter', () => {
+    it('Unit test on function with one correct argument', () => {
         let array = [1, 2, 3, 4];
-
         let result = solve(array);
         assert.equal(result, '1,2,3,4');
     });
 
-    it('Should print array with empty delimiter', () => {
+    it('Unit test on function without arguments', () => {
         let result = () => solve();
-
+        expect(result).to.throw();
+    });
+    
+    it('Unit test on function with object instead of array', () => {
+        let result = () => solve({}, '_');
         expect(result).to.throw();
     });
 });
