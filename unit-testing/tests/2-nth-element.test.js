9ee988cb4387598434c4cb336058549aa84ce33e
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-07-10 19:23:53 +0300

    test with one correct argument

--- unit-testing/tests/2-nth-element.test.js
+++ unit-testing/tests/2-nth-element.test.js
@@ -6,4 +6,10 @@ describe('2 - nth element in array', () => {
         let result = solve([1, 2, 3, 4, 5, 6], 2);
         expect(result).to.deep.equal([1, 3, 5])
     });
+
+    it('Unit test on function with one correct argument', () => {
+        let array = [1, 2, 3, 4];
+        let result = solve(array);
+        expect(result).to.deep.equal([1]);
+    });
 });
