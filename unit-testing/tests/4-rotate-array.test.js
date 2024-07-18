1daf291eea42fda929d73cb19dacccf4ee2d08d4
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-07-18 12:35:25 +0300

    test with undefined instead of rotations

--- unit-testing/tests/4-rotate-array.test.js
+++ unit-testing/tests/4-rotate-array.test.js
@@ -35,10 +35,10 @@ describe('4-Rotate array', () => {
         expect(result).to.throw();
     });
 
-    // it('Unit test on function with undefined instead of delimiter', () => {
-    //     let result = solve([1, 2, 3], undefined);
-    //     expect(result).to.equal('1,2,3')
-    // });
+    it('Unit test on function with undefined instead of rotations', () => {
+        let result = solve([1, 2, 3], undefined);
+        expect(result).to.equal('1 2 3')
+    });
 
     // it('Unit test on function with object instead of delimiter', () => {
     //     let result = solve([1, 2, 3], {});
