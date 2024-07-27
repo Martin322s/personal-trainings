e98235b29a157ad0ec331d1d49ce662c98d29b5d
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-07-27 14:06:28 +0300

    test with correct input

--- unit-testing/tests/7-sorting-numbers.test.js
+++ unit-testing/tests/7-sorting-numbers.test.js
@@ -8,11 +8,11 @@ describe('6 - List of Names', () => {
         expect(result).to.deep.equal([0, 17, 1, 12, 2, 10, 5, undefined]);
     });
 
-    // it('Unit test on function with one correct argument', () => {
-    //     let array = [1, 2, 3, 4];
-    //     let result = () => solve(array);
-    //     expect(result).to.throw();
-    // });
+    it('Unit test on function with one correct argument', () => {
+        let array = [1, 2, 3, 4];
+        let result = solve(array);
+        expect(result).to.deep.equal([1, 4, 2, 3]);
+    });
 
     // it('Unit test on function without arguments', () => {
     //     let result = () => solve();
