2478ce4644d1acc8df6154a1226f6d882802bf6d
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-07-27 14:07:26 +0300

    test with object

--- unit-testing/tests/7-sorting-numbers.test.js
+++ unit-testing/tests/7-sorting-numbers.test.js
@@ -19,10 +19,10 @@ describe('6 - List of Names', () => {
         expect(result).to.throw();
     });
 
-    // it('Unit test on function with object instead of array', () => {
-    //     let result = () => solve({}, '_');
-    //     expect(result).to.throw();
-    // });
+    it('Unit test on function with object instead of array', () => {
+        let result = () => solve({});
+        expect(result).to.throw();
+    });
 
     // it('Unit test on function with null instead of array', () => {
     //     let result = () => solve(null);
