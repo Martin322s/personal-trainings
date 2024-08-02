d10bcf100d8a5091593db1841a63a18fee31c62e
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-08-02 19:07:38 +0300

    install page library

---
+++ routing/page.js
@@ -0,0 +1,16 @@
+function renderHome() {
+    return `<h1>Home page</h1>`
+}
+
+function renderError() {
+    return `<h1>Not found!</h1>`
+}
+
+function renderAbout() {
+    return `<h1>About page</h1>`
+}
+
+function renderContacts() {
+    return `<h1>Contects page</h1>`
+}
+
