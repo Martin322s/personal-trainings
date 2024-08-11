7fcd75dca344ce854ae6fb00e78f697397d0e8ab
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-08-11 17:49:50 +0300

    mediator pattern

---
+++ design-patterns/mediator.js
@@ -0,0 +1,37 @@
+class AirTrafficControl {
+    requestTakeoff(plane) {
+        console.log(`Air Traffic Control grants takeoff clearance for ${plane.getName()}.`);
+    }
+
+    requestLanding(plane) {
+        console.log(`Air Traffic Control clears ${plane.getName()} for landing.`);
+    }
+}
+
+class Plane {
+    constructor(name) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    requestTakeoff() {
+        console.log(`${this.name} requests takeoff clearance.`);
+        airTrafficControl.requestTakeoff(this);
+    }
+
+    requestLanding() {
+        console.log(`${this.name} requests landing clearance.`);
+        airTrafficControl.requestLanding(this);
+    }
+}
+
+const airTrafficControl = new AirTrafficControl();
+
+const plane1 = new Plane('Flight 123');
+const plane2 = new Plane('Flight 456');
+
+plane1.requestTakeoff();
+plane2.requestLanding();
