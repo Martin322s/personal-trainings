0b8176b0cb6dfdf761f4c126172359227f026087
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-09-02 20:23:32 +0300

    render basic heading element

--- react-js/1-intro-to-react-jsx/jsx-syntax/app.js
+++ react-js/1-intro-to-react-jsx/jsx-syntax/app.js
@@ -1,2 +1,9 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
+
+const headingElement = React.createElement('h1', null, 'Hello from ReactJS! (Client)');
+
+root.render(headingElement);
