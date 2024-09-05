fb53798f177ccd199cf88c56f77fff57d6d62121
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-09-06 00:30:46 +0300

    ts numeric type

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,3 +3,8 @@ let isGood: boolean = true;
 let isNotGood: boolean = false;
 
 // Number type
+let integer: number = 1;
+let dobuleFloat: number = 1.10;
+let binaryNumber: number = 0x1;
+let hexaNumber: number = 0x255;
+let bigInteger: number = 0b001;
